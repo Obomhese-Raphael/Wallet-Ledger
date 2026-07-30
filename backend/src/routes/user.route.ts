e260f7e6da5a89e1cdc2d017481f986aa3824924
Raphael Obomhese <obomheser@gmail.com>
2026-07-30 21:58:11 +0100

    feat: update environment variables for Cloudinary integration; modify user routes to use updated controller methods

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 
 import { protect } from "../middleware/auth.middleware.js";
-import { getUserByEmail } from "../controllers/user.controller.js";
+import { getUserByEmail, updateAvatar } from "../controllers/user.controller.js";
 import { upload } from "../middleware/upload.middleware.js";
-import { uploadAvatar } from "../services/user.service.js";
 
 const router = Router();
 
 router.get("/find", protect, getUserByEmail);
-router.patch("/avatar", protect, upload.single("avatar"), uploadAvatar);
+router.patch("/avatar", protect, upload.single("avatar"), updateAvatar);
 
 export default router;
